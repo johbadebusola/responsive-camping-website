76eb5a6319e592cfeed9ad5f0f9cc23bd4c8f9b8
Oluwatoyin John Adebusola <johntoyin97@gmail.com>
2023-07-29 12:55:46 +0100

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,18 @@
 /*=============== SHOW MENU ===============*/
+const hamburger = document.getElementById("Hamburger");
+const menu1 = document.getElementById("menu1");
+const menu2 = document.getElementById("menu2");
+const menu3 = document.getElementById("menu3");
+const Nav = document.getElementById("navToggle")
 
-
+hamburger.addEventListener("click", () => {
+  menu1.classList.toggle("menu1");
+  menu2.classList.toggle("menu2");
+  menu3.classList.toggle("menu3");
+  Nav.classList.toggle("active")
+});
 /*=============== REMOVE MENU MOBILE ===============*/
 
-
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
 
 /*=============== GSAP ANIMATION ===============*/
